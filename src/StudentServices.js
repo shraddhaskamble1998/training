f4aecac05934d855050ad365006402fc47336b3c
shraddhaskamble1998 <82362531+shraddhaskamble1998@users.noreply.github.com>
2021-04-20 18:56:15 +0530

    Add files via upload

---
+++ src/StudentServices.js
@@ -0,0 +1,11 @@
+import axios from 'axios'
+
+const STUDENT_API_BASE_URL = "http://localhost:8844/api/students";
+
+class StudentServices {
+    getStudents() {
+        return axios.get(STUDENT_API_BASE_URL);
+    }
+}
+
+export default new StudentServices();
